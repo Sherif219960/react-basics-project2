a666e3d23b0460a5d9cb0968210b57671a211a07
sherif anwarm <sherifanwar76@gmail.com>
2023-12-18 14:11:10 +0200

    Enhancing add product modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import ProductCard from "./components/ProductCard";
 import Button from "./components/Ui/Button";
+import Input from "./components/Ui/Input";
 import Modal from "./components/Ui/Modal";
 import { FormInputList, ProductList } from "./data";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
 const App = () => {
   // STATE
@@ -24,38 +25,43 @@ const App = () => {
 
   // render FromInputList
   const RenderFromInputList = FormInputList.map((input) => (
-    <div>
+    <div className="flex flex-col" key={input.id}>
       <label
         htmlFor={input.id}
-        className="block text-sm font-medium leading-6 text-gray-900"
+        className="mt-1 text-sm font-medium leading-6 text-gray-900"
       >
-        {input.name}
+        {input.label}
       </label>
-      <input
-        type={input.type}
-        name={input.name}
-        id={input.id}
-        className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 "
-        placeholder={input.name}
-      />
+
+      <Input name={input.name} id={input.id} type={input.type} />
     </div>
   ));
 
   return (
     <div className="container mx-auto">
-      <Button className="bg-indigo-700 hover:bg-indigo-800" onClick={openModal}>
-        Add
-      </Button>
+      <div className="flex items-center  ">
+        <h2>title</h2>
+        <Button
+          className="bg-indigo-700 hover:bg-indigo-800"
+          onClick={openModal}
+        >
+          Add
+        </Button>
+      </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
         {RenderProductList}
       </div>
       <Modal isOpen={isOpen} CloseModal={closeModal} title="Add New Product">
-        {RenderFromInputList}
-        <div className="flex items-center space-x-2 my-3">
-          <Button className="bg-indigo-700 hover:bg-indigo-800">Submit</Button>
-          <Button className="bg-gray-300 hover:bg-gray-400">cancel</Button>
-        </div>
+        <form className="space-y-3">
+          {RenderFromInputList}
+          <div className="flex items-center space-x-3 ">
+            <Button className="bg-indigo-700 hover:bg-indigo-800">
+              Submit
+            </Button>
+            <Button className="bg-gray-300 hover:bg-gray-400">cancel</Button>
+          </div>
+        </form>
       </Modal>
     </div>
   );
